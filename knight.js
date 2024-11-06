f952178d3cbca184106bcd58b464345db86216aa
Sky Randy <soras1758@gmail.com>
2024-11-06 23:46:37 +0900

    improve variable naming

---
+++ knight.js
@@ -0,0 +1,49 @@
+function knightMoves(start, end) {
+  const moves = [
+      [2, 1], [2, -1], [-2, 1], [-2, -1],
+      [1, 2], [1, -2], [-1, 2], [-1, -2]
+  ];
+
+  function bfs(start, end) {
+      const queue = [[start, [start]]];
+      const visited = new Set();
+
+      while (queue.length) {
+          const [currentPosition, path] = queue.shift();
+          const [x, y] = currentPosition;
+
+          if (x === end[0] && y === end[1]) {
+              return path;
+          }
+
+          visited.add(`${x},${y}`);
+
+          const nextMoves = movePos(currentPosition, moves);
+
+          for (let next of nextMoves) {
+              const [nextX, nextY] = next;
+              const posKey = `${nextX},${nextY}`;
+
+              if (!visited.has(posKey)) {
+                  queue.push([next, [...path, next]]);
+              }
+          }
+      }
+
+      return null;
+  }
+
+  return bfs(start, end);
+}
+
+function movePos(currentPos, moves) {
+  const result = [];
+  moves.forEach((move) => {
+      const x = currentPos[0] + move[0];
+      const y = currentPos[1] + move[1];
+      if (x >= 0 && x < 8 && y >= 0 && y < 8) {
+          result.push([x, y]);
+      }
+  });
+  return result;
+}
